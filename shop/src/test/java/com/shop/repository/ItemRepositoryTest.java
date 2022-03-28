81de15875e4d2b7b26948b7ece241bec5f19ab44
kyojunichi <junjundesuu@yahoo.co.jp>
2022-03-29 00:23:52 +0900

    商品 Repository 作成 및 테스트。

---
+++ shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("商品 貯藏 테스트")
+    public void createItemTest() {
+        Item item = new Item();
+        item.setItemNm("테스트 商品");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 商品 詳細 說明");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+    }
+}

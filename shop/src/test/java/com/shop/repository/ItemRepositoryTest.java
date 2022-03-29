15b43b98344ec997846894012ba216a51c65893a
kyojunichi <junjundesuu@yahoo.co.jp>
2022-03-29 17:14:53 +0900

    쿼리 메소드를 利用하여 商品을 照會해보기。

--- shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -30,5 +31,59 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem.toString());
+    }
+
+    public void createItemList() {
+        for (int i=1; i<=10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 商品" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 商品 詳細　說明" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("商品名 照會 테스트")
+    public void findByItemNmTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 商品1");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("商品名、商品詳細說明 or 테스트")
+    public void findByItemNmOrItemDetail() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 商品1", "테스트 商品 詳細 說明5");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("價格 LessThan 테스트")
+    public void findByPriceLessThanTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("價格 내림次順 照會 테스트")
+    public void findByPriceLessThanOrderByPriceDesc() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 }
